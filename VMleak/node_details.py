481d1bdf7fa219ddd24edf759ab4197ece03796e
trent-efi <trent.efi@gmail.com>
2015-09-25 17:21:45 -0700

    Friday end of the day. node_details.py: TODO, popup displaying stuff

--- VMleak/node_details.py
+++ VMleak/node_details.py
@@ -4,4 +4,8 @@ num = len(sys.argv)
 if num < 3 or num > 3:
     print "UNKNOWN SYSTEM ERROR:"
 else:
-    print "UP IN HEWRERERERER " + str(len(sys.argv))
+    name = sys.argv[1]
+    index = sys.argv[2]
+
+    print "UP IN HEWRERERERER " + str(name) + " " + str(index) 
+#end else
